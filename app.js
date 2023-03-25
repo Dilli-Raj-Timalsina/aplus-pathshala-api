91a47c23b2e670636b0864678e5251b1ccafb5b2
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-03-25 17:02:07 +0545

    ok

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const app = express();
-const userRouter = express.Router();
-const { loginUser, getUser } = require("./Controller/userController");
+const studentRouter = express.Router();
+const {
+  loginControl,
+  signupControl,
+} = require("./Controller/studentController.js");
 
 app.use(cookieParser());
 //It is used to make req.body available ,-----
 //it is middleware so it should be added in middleware stack
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-app.use("/api/v1/user", userRouter);
-userRouter.route("/login").post(loginUser).get(getUser);
-userRouter.route("/:id").post(loginUser).get(getUser);
+app.use("/api/v1/student", userRouter);
+studentRouter.route("/signup").post(loginUser).get(getUser);
+studentRouter.route("/login").post(loginUser).get(getUser);
 
 //unhandled route:
 app.all("*", (req, res, next) => {
