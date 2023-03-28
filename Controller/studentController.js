05ca2f543553dd08509a4a189e455a90d341ab03
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-03-28 15:18:50 +0545

    done2

--- Controller/studentController.js
+++ Controller/studentController.js
@@ -1,10 +1,28 @@
 const jwt = require("jsonwebtoken");
 const Student = require("../Model/studentSchema");
 const catchAsync = require("../ErrorHandeling/catchAsync");
+const appError = require("./../ErrorHandeling/appError");
 const bcrypt = require("bcrypt");
+const AppError = require("./../ErrorHandeling/appError");
 
 const loginControl = catchAsync(async (req, res) => {
-  throw new Error("thrown error");
+  const { email, passport } = req.body;
+  //check if email or passport exist:
+  if (!email || !passport) {
+    throw new AppError("email or passport not provided", 400);
+  }
+  const query = await Student.findOne({ email: email }).select("passport");
+  const match = await bcrypt.compare(passport, query.passport);
+  console.log(query);
+  console.log(match);
+  if (match) {
+    res.status(400).json({
+      status: "sucess",
+      result: `Correct passport welcome ${email.split("@")[0]}`,
+    });
+  } else {
+    throw new AppError("Incorrect Passport", 200);
+  }
 });
 
 const signupControl = catchAsync(async (req, res) => {
@@ -12,22 +30,14 @@ const signupControl = catchAsync(async (req, res) => {
   req.body.passport = hash;
   req.body.passport_confirm = hash;
   const data = new Student(req.body);
-  data
-    .save()
-    .then((savedDoc) => {
-      res.json({
-        status: "sucess",
-        data: {
-          savedDoc,
-        },
-      });
-    })
-    .catch((error) => {
-      res.json({
-        status: "Failed",
-        message: { error },
-      });
+  data.save().then((savedDoc) => {
+    res.status(200).json({
+      status: "sucess",
+      data: {
+        savedDoc,
+      },
     });
+  });
 });
 
 const dashBoard = catchAsync(async (req, res) => {
