6fe9f6ce4ed26277a02ec2f02ff6bf7e2d1d1599
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-04-14 19:30:24 +0545

    implement rate limiter for ddos attack and bruteforce

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const app = express();
 const passport = require("passport");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+//Global Middlewares:
 
+//Rate Limiter:
+const rateLimit = require("express-rate-limit");
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use("/api", rateLimit);
 //global error handler config:
 const AppError = require("./errors/appError");
 const globalErrorHandler = require("./errors/errorController");
--- controllers/studentAuthController.js
+++ controllers/studentAuthController.js
@@ -14,6 +14,31 @@ const signToken = async (id) => {
   });
 };
 
+//This function sends cookie to brower so that is saves the cookie and send in next subsequent request
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+  res.cookie("jwt", token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
+
 const loginControl = catchAsync(async (req, res) => {
   const { email, password } = req.body;
 
