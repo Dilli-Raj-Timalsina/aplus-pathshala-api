a31a5dfbf642368295df8e4c8f600d2d0664de9f
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-04-15 17:30:13 +0545

    course schema added

--- models/courseSchema.js
+++ models/courseSchema.js
@@ -2,12 +2,34 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const courseSchema = new Schema({
-  courseName: {
+  Sector: {
+    required: true,
+    type: String,
+  },
+  Category: {
+    required: true,
+    type: String,
+  },
+  Duration: {
+    type: Number,
     required: true,
   },
-  duration: Number,
-  description: String,
+  Price: {
+    required: true,
+    type: Number,
+  },
+  Description: {
+    required: true,
+    type: String,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  student: [{ type: Schema.Types.ObjectId, ref: "Student", required: false }],
+  review: [{ type: Schema.Types.ObjectId, ref: "Review", required: false }],
 });
 
 const Course = mongoose.model("Course", courseSchema);
+
 module.exports = Course;
