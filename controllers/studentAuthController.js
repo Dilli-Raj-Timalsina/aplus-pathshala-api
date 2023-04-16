e312d3e5bf821d84f92a2da2d1d01cb48f0cc2b5
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-04-16 14:27:20 +0545

    logout function implemented

--- controllers/studentAuthController.js
+++ controllers/studentAuthController.js
@@ -134,6 +134,15 @@ const resetControl = catchAsync(async (req, res) => {
 
 //just for testing purpose
 const logoutControl = catchAsync(async (req, res, next) => {
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+  res.cookie("jwt", "xxx", cookieOptions);
   res.end("logged out");
 });
 
