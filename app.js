81716b9d01f7a1cf0b8d52d702f97d395b5f4894
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-04-23 14:13:08 +0545

    config setting

--- app.js
+++ app.js
@@ -1,59 +1,59 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
 //global error handler config:
-const AppError = require("./errors/appError");
-const globalErrorHandler = require("./errors/errorController");
+const AppError = require("./errors/appError")
+const globalErrorHandler = require("./errors/errorController")
 
 //making req.body available:
-const bodyParser = require("body-parser");
-app.use(bodyParser.json());
+const bodyParser = require("body-parser")
+app.use(bodyParser.json())
 
 // By default, $ and . characters are removed completely and used for query injection protection:
-const mongoSanitize = require("express-mongo-sanitize");
-app.use(mongoSanitize());
+const mongoSanitize = require("express-mongo-sanitize")
+app.use(mongoSanitize())
 
 //It helps you secure your Express apps by setting various HTTP headers :
-const helmet = require("helmet");
-app.use(helmet());
+const helmet = require("helmet")
+app.use(helmet())
 
 // This will sanitize any data in req.body, req.query, and req.params for xss attack :
-const xss = require("xss-clean");
-app.use(xss());
+const xss = require("xss-clean")
+app.use(xss())
 
 //It helps for protecting bruteforce attack and DDOS attack by limiting no of request per IP address:
-const rateLimit = require("express-rate-limit");
+const rateLimit = require("express-rate-limit")
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-});
-app.use("/api", limiter);
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+app.use("/api", limiter)
 
 // passport configuration:
-const passport = require("passport");
-require("./authConfig/passport-jwt");
-require("./authConfig/passport-google");
-app.use(passport.initialize());
+const passport = require("passport")
+require("./authConfig/passport-jwt")
+require("./authConfig/passport-google")
+app.use(passport.initialize())
 
 //Routes:
-const googleAuthRouter = require("./routes/googleAuthRouter");
-const studentRouter = require("./routes/studentRouter");
-const teacherRouter = require("./routes/teacherRouter");
-const courseRouter = require("./routes/courseRouter");
+const googleAuthRouter = require("./routes/googleAuthRouter")
+const studentRouter = require("./routes/studentRouter")
+const teacherRouter = require("./routes/teacherRouter")
+const courseRouter = require("./routes/courseRouter")
 
-app.use(googleAuthRouter);
-app.use("/api/v1/student", studentRouter);
-app.use("/api/v1/teacher", teacherRouter);
-app.use("/api/v1/course", courseRouter);
+app.use(googleAuthRouter)
+app.use("/api/v1/student", studentRouter)
+app.use("/api/v1/teacher", teacherRouter)
+app.use("/api/v1/course", courseRouter)
 
 //handeling global unhandled error and rejection ,e.g if no route is defined for certain url
 app.all("*", (req, res, next) => {
-  // console.log("hello");
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-});
+    // console.log("hello");
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
+})
 
-app.use(globalErrorHandler);
+app.use(globalErrorHandler)
 
-module.exports = app;
+module.exports = app
