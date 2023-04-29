5102a9ba29c2c077c78c2f6662888ae267b9d8ac
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-04-29 19:33:52 +0545

    file upload related tass

--- awsConfig/multerSetup.js
+++ awsConfig/multerSetup.js
@@ -5,33 +5,33 @@ const AppError = require("../errors/appError");
 // /*
 // setup for Disk storage/to store on your current pc/machine
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "uploads");
-    },
-    filename: function (req, file, cb) {
-        const uniqueSuffix = Date.now() + "." + file.mimetype.split("/")[1];
-        cb(null, file.fieldname + "-" + uniqueSuffix);
-    },
-});
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, "./uploads");
+//     },
 
-const fileFilter = (req, file, cb) => {
-    if (file.mimetype.split("/")[0] === "image") {
-        cb(null, true);
-    } else {
-        cb(new AppError("Only Image can be inserted"), false);
-    }
-};
-const upload = multer({ dest: "./uploads" });
-// */
-
-//setup for cloud storage / to create a buffer
-// const storage = multer.memoryStorage();
-
-// const upload = multer({
-//     storage,
-//     // fileFilter,
-//     // limits: { fileSize: 1000000000 },
+//     filename: function (req, file, cb) {
+//         cb(null, Date.now() + "-" + file.originalname);
+//     },
 // });
+
+// const fileFilter = (req, file, cb) => {
+//     if (file.mimetype.split("/")[0] === "image") {
+//         cb(null, true);
+//     } else {
+//         cb(new AppError("Only Image can be inserted"), false);
+//     }
+// };
+// const upload = multer({ storage });
+// // */
+
+// setup for cloud storage / to create a buffer
+const storage = multer.memoryStorage();
+
+const upload = multer({
+    storage,
+    // fileFilter,
+    // limits: { fileSize: 1000000000 },
+});
 
 module.exports = upload;
