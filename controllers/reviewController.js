64856771c532c5597f4dcb504daf910b9f0982e3
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-05-05 16:50:58 +0545

    error wrong text to "developement" refactored

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,10 +1,35 @@
 const Course = require("./../models/courseSchema");
 const Review = require("./../models/reviewSchema");
-const writeReview = async (req, res, next) => {
+const AppError = require("./../errors/appError");
+const catchAsync = require("./../errors/catchAsync");
+const writeReview = catchAsync(async (req, res, next) => {
+    throw new AppError("This is wrong bro");
     const { bucketName } = req.body;
     const review = {
         rating: "5",
         comment: "Thi is comment",
+    };
+
+    const doc = await Course.findOneAndUpdate(
+        {
+            bucketName: bucketName,
+        },
+        {
+            $push: {
+                review: review,
+            },
+        }
+    );
+
+    res.end("review successful");
+});
+
+const editReview = async (req, res, next) => {
+    const { bucketName } = req.body;
+    const review = {
+        rating: "5",
+        comment: "Thi is comment",
+        updatedAt: Date.now(),
     };
 
     const doc = await Course.findOneAndUpdate(
