31b074ea1cab5dcbf86eaf751e6da95192312f03
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-05-06 10:26:01 +0545

    prodtion ready by server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = require("./app");
 
 //setting dotenv file in our server
 require("dotenv").config({ path: __dirname + "/.env" });
-
+const port = process.env.PORT || 3000;
 //MongoDB local Database Connection:
 try {
-    mongoose.connect("mongodb://127.0.0.1:27017/User");
+    mongoose.connect(process.env.CONN);
     console.log("Data base connection succesfull");
 } catch (err) {
     console.log(err);
@@ -15,8 +15,8 @@ try {
 when we run "node server.js" it imports app.js i.e object of module.exports,
 and creates app{} object from express() method and starts server by listening
 to the port 3000/PORT , by below code .
-*/ //
+*/
 
-app.listen(process.env["PORT"], () => {
+app.listen(port, () => {
     console.log(`App is running on port ${process.env.PORT}`);
 });
