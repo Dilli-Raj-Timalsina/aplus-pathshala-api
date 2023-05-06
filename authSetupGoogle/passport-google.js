09e10fb7cbf1da6022374b076646a31810ef90c5
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-05-06 11:56:21 +0545

    callback uri changed to production

--- authSetupGoogle/passport-google.js
+++ authSetupGoogle/passport-google.js
@@ -4,37 +4,38 @@ require("dotenv").config();
 const User = require("../models/studentSchema");
 
 passport.use(
-  new GoogleStrategy(
-    {
-      // options for google strategy
-      clientID: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "/auth/google/redirect",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      // check if user already exists in our own db
-      User.findOne({
-        google_id: (profile.id = 100),
-        email: profile.emails[0].value,
-      }).then((currentUser) => {
-        if (currentUser) {
-          // already have this user
-          done(null, currentUser);
-        } else {
-          // if not, create user in our db
-          new User({
-            google_id: profile.id,
-            name: profile.displayName,
-            email: profile.emails[0].value,
-            passport: "google",
-            passport_confirm: "google",
-          })
-            .save()
-            .then((newUser) => {
-              done(null, newUser);
+    new GoogleStrategy(
+        {
+            // options for google strategy
+            clientID: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
+            callbackURL:
+                "https://a-pathshala-service-2.onrender.com/auth/google/redirect",
+        },
+        (accessToken, refreshToken, profile, done) => {
+            // check if user already exists in our own db
+            User.findOne({
+                google_id: (profile.id = 100),
+                email: profile.emails[0].value,
+            }).then((currentUser) => {
+                if (currentUser) {
+                    // already have this user
+                    done(null, currentUser);
+                } else {
+                    // if not, create user in our db
+                    new User({
+                        google_id: profile.id,
+                        name: profile.displayName,
+                        email: profile.emails[0].value,
+                        passport: "google",
+                        passport_confirm: "google",
+                    })
+                        .save()
+                        .then((newUser) => {
+                            done(null, newUser);
+                        });
+                }
             });
         }
-      });
-    }
-  )
+    )
 );
