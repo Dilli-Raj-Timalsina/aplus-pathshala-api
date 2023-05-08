41a0181cf26e0af545e7c021f4d429b7dedf3ffb
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-05-08 20:44:44 +0545

    responce

--- controllers/createCourseController.js
+++ controllers/createCourseController.js
@@ -143,23 +143,29 @@ const createNewCourse = catchAsync(async (req, res, next) => {
     //database work:
     //create new course in database with thumbnail reference to s3
     const { bucketName } = req.body;
-    console.log(req.body, req.file, req.files);
+    console.log(req.body, req.file);
     const thumbnailKey = `${Date.now()}-${req.file.originalname}`;
 
-    await Course.create({ ...req.body, thumbnail: thumbnailKey });
+    const doc = await Course.create(
+        { ...req.body, thumbnail: thumbnailKey },
+        { new: true }
+    );
 
     // cloud work:
     // create a new course bucket
     await createBucket({ Bucket: bucketName });
 
     // upload thumbnail in s3
-    // const command = new PutObjectCommand({
-    //     Bucket: bucketName,
-    //     Key: thumbnailKey,
-    //     Body: req.file.buffer,
-    // });
-    // await s3.send(command);
-    res.end("New Course Successfully created");
+    const command = new PutObjectCommand({
+        Bucket: bucketName,
+        Key: thumbnailKey,
+        Body: req.file.buffer,
+    });
+    await s3.send(command);
+    res.status(200).json({
+        status: "success",
+        doc,
+    });
 });
 
 module.exports = {
